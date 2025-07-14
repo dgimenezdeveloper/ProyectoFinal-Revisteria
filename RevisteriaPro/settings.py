e81147fe394ec7b698bfb70435d4de8b60033eea
Dario <dgimenez.developer@gmail.com>
2025-07-14 20:52:27 -0300

    Configuración final para despliegue

--- RevisteriaPro/settings.py
+++ RevisteriaPro/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1b&zn*ammq%8i6y=y&7dio_6%@)5r&)8s+s1$qcw40u02*1-^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['antiguarevisteria.pythonanywhere.com'] 
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
